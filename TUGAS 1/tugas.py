e048e43f77cf8cbea807a13733e453910ed89eb1
syarifanajwa <161546194+syarifanajwa@users.noreply.github.com>
2024-09-30 21:03:42 +0700

    Add files via upload

---
+++ TUGAS 1/tugas.py
@@ -0,0 +1,6 @@
+total_belanja = int(input("Masukkan total belanja:"))
+
+if total_belanja > 100000:
+    print('Selamat, Anda dapat hadiah!')
+else:
+    print('Terimakasih sudah berbelanja')
---
+++ TUGAS 1/tugas2.py
@@ -0,0 +1,11 @@
+password_benar = "rahasia123"
+
+
+password = input("Masukkan password: ")
+
+if password == password_benar:
+    print("Selamat datang bos!")
+else:
+    print("Password salah, coba lagi!")
+    
+print("Terimakasih sudah menggunakan aplikasi kami.")
